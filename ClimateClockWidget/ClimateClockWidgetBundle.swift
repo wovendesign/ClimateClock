b494378ca922520c906f57f3164766817d4c7605
Eric Wätke <eric.waetke@fh-potsdam.de>
2024-05-16 11:04:59 +0200

    New Widget System

---
+++ ClimateClockWidget/ClimateClockWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateClockWidgetBundle.swift
+//  ClimateClockWidget
+//
+//  Created by Eric Wätke on 15.05.24.
+//  Copyright © 2024 woven. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ClimateClockWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ClimateClockWidget()
+    }
+}
