26a19d9a19acd6bde6ee59fb26a84a75261835a4
Eric Wätke <eric.waetke@fh-potsdam.de>
2024-05-18 18:43:10 +0200

    Added Renewables Widget

--- ClimateClockWidget/Widgets/RenewablesWidget.swift
+++ ClimateClockWidget/Widgets/RenewablesWidget.swift
@@ -11,25 +11,25 @@ import SwiftUI
 
 struct RenewablesProvider: TimelineProvider {
 	func placeholder(in context: Context) -> RenewablesEntry {
-		RenewablesEntry(date: Date(), emoji: "😀")
+		RenewablesWidgetMockData().mockEntry
 	}
-
+	
 	func getSnapshot(in context: Context, completion: @escaping (RenewablesEntry) -> ()) {
-		let entry = RenewablesEntry(date: Date(), emoji: "😀")
+		let entry = RenewablesWidgetMockData().mockEntry
 		completion(entry)
 	}
-
+	
 	func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
 		var entries: [RenewablesEntry] = []
-
+		
 		// Generate a timeline consisting of five entries an hour apart, starting from the current date.
 		let currentDate = Date()
 		for hourOffset in 0 ..< 5 {
 			let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-			let entry = RenewablesEntry(date: entryDate, emoji: "😀")
+			let entry = RenewablesWidgetMockData().mockEntry
 			entries.append(entry)
 		}
-
+		
 		let timeline = Timeline(entries: entries, policy: .atEnd)
 		completion(timeline)
 	}
@@ -37,26 +37,73 @@ struct RenewablesProvider: TimelineProvider {
 
 struct RenewablesEntry: TimelineEntry {
 	let date: Date
-	let emoji: String
+	let timestamp: Date
+	let initial: Double
+	let rate: Double
+	let precision: Int
+	
+	init(date: Date, timestamp: String, initial: Double, rate: Double, precision: Int) {
+		let dateFormatter = ISO8601DateFormatter()
+		
+		let pastTimestamp = dateFormatter.date(from: timestamp) ?? Date()
+		
+		self.date = date
+		self.timestamp = pastTimestamp
+		self.initial = initial
+		self.rate = rate
+		self.precision = precision
+	}
 }
 
 struct RenewablesWidgetEntryView : View {
 	var entry: RenewablesProvider.Entry
-
+	
 	var body: some View {
-		VStack {
-			Text("Time:")
-			Text(entry.date, style: .time)
-
-			Text("Emoji:")
-			Text(entry.emoji)
+		VStack(alignment: .center, spacing: 0) {
+			Text("\(valueByDate(date: entry.date), specifier: "%0.\(entry.precision)f") %")
+				.font(
+					.custom("Oswald", size: 20)
+					.weight(.medium)
+				)
+				.monospacedDigit()
+//				.minimumScaleFactor(0.5)
+				.lineLimit(1)
+				.contentTransition(
+					.numericText(
+						value: valueByDate(date: entry.date)
+					)
+				)
+			
+			Rectangle()
+				.containerRelativeFrame(.horizontal)
+				.frame(height: 25)
+				.border(.white, width: 4)
+				.foregroundStyle(.windowBackground)
+				.overlay(alignment: .leading) {
+					Rectangle()
+						.containerRelativeFrame(.horizontal) { length, _ in
+							return length * (valueByDate(date: entry.date) / 100)
+						}
+				}
+				.clipShape(.rect(cornerRadius: 6))
+			
+			Text("Energy from renewables")
+				.font(
+					.custom("Assistant", size: 14)
+					.weight(.semibold)
+				)
 		}
+	}
+	func valueByDate(date: Date) -> Double {
+		let timeDifference = date.timeIntervalSince(entry.timestamp)
+		
+		return entry.initial + timeDifference * (entry.rate)
 	}
 }
 
 struct RenewablesWidget: Widget {
 	let kind: String = "RenewablesWidget"
-
+	
 	var body: some WidgetConfiguration {
 		StaticConfiguration(kind: kind, provider: RenewablesProvider()) { entry in
 			if #available(iOS 17.0, *) {
@@ -70,14 +117,14 @@ struct RenewablesWidget: Widget {
 		}
 		.configurationDisplayName("Renewables Widget")
 		.description("This is an example widget.")
-		.supportedFamilies([.accessoryInline, .accessoryCircular, .accessoryRectangular])
+		.supportedFamilies([.accessoryRectangular])
 	}
 }
 
 #Preview(as: .accessoryRectangular) {
 	RenewablesWidget()
 } timeline: {
-	RenewablesEntry(date: .now, emoji: "😀")
+	RenewablesWidgetMockData().mockEntry
 }
 //
 //#Preview("Watch", as: .accessoryRectangular) {
@@ -85,3 +132,11 @@ struct RenewablesWidget: Widget {
 //} timeline: {
 //	RenewablesEntry(date: .now, emoji: "😀")
 //}
+
+struct RenewablesWidgetMockData {
+	let mockEntry = RenewablesEntry(date: Date(),
+									timestamp: "2020-01-01T00:00:00+00:00",
+									initial: 11.4,
+									rate: 2.0428359571070087e-8,
+									precision: 9)
+}
