254ef796076b86ec20c822ff995ee7682f1eaf4c
Eric Wätke <eric.waetke@fh-potsdam.de>
2024-07-03 11:35:54 +0200

    Renewable Widget Styling

--- ClimateClock Watch App/Tabs/Deadline/CountdownView.swift
+++ ClimateClock Watch App/Tabs/Deadline/CountdownView.swift
@@ -9,21 +9,10 @@ import SwiftUI
 
 struct CountdownView: View {
     var body: some View {
-        //		let watchFaceUrl = Bundle.main.url(forResource: "Solar Analogue", withExtension: "watchface")
         VStack {
             Text("Todo: Countdown View")
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-            //			Button(action: {
-            //				//An object for importing watch faces that the app provides
-            //				let library = CLKWatchFaceLibrary()
-            //				library.addWatchFace(at: watchFaceUrl!) { (_: Error?) in
-            //					// Handle the error here
-            //				}
-            //			}, label: {
-            //				Text("Add watchface")
-            //			})
         }
-        //		.frame(minWidth: .infinity, minHeight: .infinity)
 
         .background(
             LinearGradient(gradient: Gradient(colors: [.aubergine, .black]),
--- Widgets/Views/RenewablesWidget.swift
+++ Widgets/Views/RenewablesWidget.swift
@@ -10,132 +10,139 @@ import SwiftUI
 import WidgetKit
 
 struct RenewablesProvider: TimelineProvider {
-    func placeholder(in _: Context) -> RenewablesEntry {
-        RenewablesWidgetMockData().mockEntry
-    }
-
-    func getSnapshot(in _: Context, completion: @escaping (RenewablesEntry) -> Void) {
-        let entry = RenewablesWidgetMockData().mockEntry
-        completion(entry)
-    }
-
-    func getTimeline(in _: Context, completion: @escaping (Timeline<Entry>) -> Void) {
-        var entries: [RenewablesEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = RenewablesWidgetMockData().mockEntry
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
+	func placeholder(in _: Context) -> RenewablesEntry {
+		RenewablesWidgetMockData().mockEntry
+	}
+	
+	func getSnapshot(in _: Context, completion: @escaping (RenewablesEntry) -> Void) {
+		let entry = RenewablesWidgetMockData().mockEntry
+		completion(entry)
+	}
+	
+	func getTimeline(in _: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+		var entries: [RenewablesEntry] = []
+		
+		// Generate a timeline consisting of five entries an hour apart, starting from the current date.
+		let currentDate = Date()
+		for hourOffset in 0 ..< 5 {
+			let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+			let entry = RenewablesWidgetMockData().mockEntry
+			entries.append(entry)
+		}
+		
+		let timeline = Timeline(entries: entries, policy: .atEnd)
+		completion(timeline)
+	}
 }
 
 struct RenewablesEntry: TimelineEntry {
-    let date: Date
-    let timestamp: Date
-    let initial: Double
-    let rate: Double
-    let precision: Int
-
-    init(date: Date, timestamp: String, initial: Double, rate: Double, precision: Int) {
-        let dateFormatter = ISO8601DateFormatter()
-
-        let pastTimestamp = dateFormatter.date(from: timestamp) ?? Date()
-
-        self.date = date
-        self.timestamp = pastTimestamp
-        self.initial = initial
-        self.rate = rate
-        self.precision = precision
-    }
+	let date: Date
+	let timestamp: Date
+	let initial: Double
+	let rate: Double
+	let precision: Int
+	
+	init(date: Date, timestamp: String, initial: Double, rate: Double, precision: Int) {
+		let dateFormatter = ISO8601DateFormatter()
+		
+		let pastTimestamp = dateFormatter.date(from: timestamp) ?? Date()
+		
+		self.date = date
+		self.timestamp = pastTimestamp
+		self.initial = initial
+		self.rate = rate
+		self.precision = precision
+	}
 }
 
 struct RenewablesWidgetEntryView: View {
-    var entry: RenewablesProvider.Entry
-
-    var body: some View {
+	var entry: RenewablesProvider.Entry
+	@Environment(\.showsWidgetContainerBackground) var showsBackground
+	
+	var body: some View {
 		VStack(alignment: .leading, spacing: 0) {
-            Text("\(valueByDate(date: entry.date), specifier: "%0.\(entry.precision)f") %")
-                .font(
-                    .custom("Oswald", size: 20)
-                        .weight(.semibold)
-                )
-                .monospacedDigit()
-                .lineLimit(1)
-                .contentTransition(
-                    .numericText(
-                        value: valueByDate(date: entry.date)
-                    )
-                )
-
-            RoundedRectangle(cornerRadius: 6)
-                .containerRelativeFrame(.horizontal)
-				.frame(height: 12)
-                .foregroundStyle(.windowBackground)
-				.opacity(0.5)
-				.overlay {
-					RoundedRectangle(cornerRadius: 6)
-						.stroke(lineWidth: 2)
-						.foregroundStyle(.foreground)
-				}
-                .overlay(alignment: .leading) {
-                    Rectangle()
-                        .containerRelativeFrame(.horizontal) { length, _ in
-                            length * (valueByDate(date: entry.date) / 100)
-                        }
-						.clipShape(.rect(cornerRadius: 6))
-						.padding(2)
-						.foregroundStyle(.lime)
-						.widgetAccentable()
-                }
-                .clipShape(.rect(cornerRadius: 6))
-
-            Text("Energy from renewables")
-                .textCase(.uppercase)
-                .font(
-                    .custom("Assistant", size: 12)
-                        .weight(.semibold)
-                )
-        }
-		.padding(.horizontal, 6)
-		.containerBackground(.blue, for: .widget)
-    }
-
-    func valueByDate(date: Date) -> Double {
-        let timeDifference = date.timeIntervalSince(entry.timestamp)
-
-        return entry.initial + timeDifference * (entry.rate)
-    }
+			Text("\(valueByDate(date: entry.date), specifier: "%0.\(entry.precision)f") %")
+				.font(
+					.custom("Oswald", size: 20)
+					.weight(.semibold)
+				)
+				.monospacedDigit()
+				.lineLimit(1)
+				.contentTransition(
+					.numericText(
+						value: valueByDate(date: entry.date)
+					)
+				)
+				.foregroundStyle(.lifelineFg1)
+			
+			RoundedRectangle(cornerRadius: 3)
+							.frame(height: 21)
+							.foregroundStyle(.foreground.opacity(0.25))
+							.overlay(alignment: .leading) {
+								GeometryReader { geometry in
+									Rectangle()
+										.frame(width: geometry.size.width * (valueByDate(date: entry.date) / 100), height: 21)
+										.foregroundStyle(.lifelineFg1)
+										.clipShape(RoundedRectangle(cornerRadius: 3))
+										.widgetAccentable()
+								}
+							}
+			
+			Spacer()
+			Text("Energy from renewables")
+				.textCase(.uppercase)
+				.font(
+					.custom("Assistant", size: 12)
+					.weight(.semibold)
+				)
+		}
+		.padding(EdgeInsets(top: 0, leading: 6, bottom: 6, trailing: 6))
+		.containerRelativeFrame(.horizontal)
+		.containerRelativeFrame(.vertical)
+		.widgetBackground(LinearGradient(colors: [.lifelineBg1, .lifelineBg2],
+										 startPoint: .top,
+										 endPoint: .bottom))
+		.background {
+			if (showsBackground) {
+				LinearGradient(colors: [.lifelineBg1, .lifelineBg2],
+							   startPoint: .top,
+							   endPoint: .bottom)
+			} else {
+				Color.clear
+			}
+		}
+	}
+	
+	func valueByDate(date: Date) -> Double {
+		let timeDifference = date.timeIntervalSince(entry.timestamp)
+		
+		return entry.initial + timeDifference * (entry.rate)
+	}
 }
 
 struct RenewablesWidget: Widget {
-    let kind: String = "RenewablesWidget"
-
-    var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: RenewablesProvider()) { entry in
-            if #available(iOS 17.0, *) {
-                RenewablesWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
-//					.containerBackground(.blue, for: .widget)
-            } else {
-                RenewablesWidgetEntryView(entry: entry)
-                    .padding()
-                    .background()
-            }
-        }
-        .configurationDisplayName("Renewables Widget")
-        .description("This is an example widget.")
-		#if os(watchOS)
-			.supportedFamilies([.accessoryRectangular])
-		#else
-			.supportedFamilies([.accessoryRectangular, .systemSmall])
-		#endif
-    }
+	let kind: String = "RenewablesWidget"
+	
+	var body: some WidgetConfiguration {
+		StaticConfiguration(kind: kind, provider: RenewablesProvider()) { entry in
+			if #available(iOS 17.0, *) {
+				RenewablesWidgetEntryView(entry: entry)
+					.containerBackground(.fill.tertiary, for: .widget)
+				//					.containerBackground(.blue, for: .widget)
+			} else {
+				RenewablesWidgetEntryView(entry: entry)
+					.padding()
+					.background()
+			}
+		}
+		.configurationDisplayName("Renewables Widget")
+		.description("This is an example widget.")
+#if os(watchOS)
+		.supportedFamilies([.accessoryRectangular])
+#else
+		.supportedFamilies([.accessoryRectangular, .systemSmall])
+#endif
+	}
 }
 
 //#Preview(as: .systemSmall) {
@@ -152,9 +159,22 @@ struct RenewablesWidget: Widget {
 // }
 
 struct RenewablesWidgetMockData {
-    let mockEntry = RenewablesEntry(date: Date(),
-                                    timestamp: "2020-01-01T00:00:00+00:00",
-                                    initial: 11.4,
-                                    rate: 2.0428359571070087e-8,
-                                    precision: 6)
+	let mockEntry = RenewablesEntry(date: Date(),
+									timestamp: "2020-01-01T00:00:00+00:00",
+									initial: 11.4,
+									rate: 2.0428359571070087e-8,
+									precision: 6)
+}
+
+
+extension View {
+	func widgetBackground(_ backgroundView: some View) -> some View {
+		if #available(iOSApplicationExtension 17.0, *) {
+			return containerBackground(for: .widget) {
+				backgroundView
+			}
+		} else {
+			return background(backgroundView)
+		}
+	}
 }
