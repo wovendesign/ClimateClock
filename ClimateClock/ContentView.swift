ba8ccd9fdbe4680abe4adeb0d5b75a8a23c5ba0f
Eric Wätke <eric.waetke@fh-potsdam.de>
2024-07-03 18:04:00 +0200

    Added Idea Sheet for iOS App

--- ClimateClock/ContentView.swift
+++ ClimateClock/ContentView.swift
@@ -9,20 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("The Climate Clock iOS App is still in development. Please open the Climate Clock App on your Apple Watch.")
-        }
-        .padding()
-//		NavigationStack {
-//			IdeaList(list: [
-//				Idea(id: UUID(), status: .approved, date_created: "", idea: "Have a plattform for people to connect and share ideas"),
-//			 Idea(id: UUID(), status: .approved, date_created: "", idea: "Why isnt there a list of upcoming protests")
-//		 ])
-//			.navigationTitle("Climate Clock")
-//		}
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("The Climate Clock iOS App is still in development. Please open the Climate Clock App on your Apple Watch.")
+//        }
+//        .padding()
+		NavigationStack {
+			IdeaList(list: [
+				Idea(id: UUID(), status: .approved, date_created: "", idea: "Have a plattform for people to connect and share ideas"),
+			 Idea(id: UUID(), status: .approved, date_created: "", idea: "Why isnt there a list of upcoming protests")
+		 ])
+			.navigationTitle("Climate Clock")
+		}
     }
 }
 
--- ClimateClock/Views/IdeaList.swift
+++ ClimateClock/Views/IdeaList.swift
@@ -9,15 +9,76 @@
 import SwiftUI
 
 struct IdeaList: View {
-	let list: [Idea]
+	@State var list: [Idea]
 	@State private var searchText: String = ""
+	@State private var showingSheet = false
 	
-    var body: some View {
-		List(list) { idea in
-			Text(idea.idea)
+	
+	var filteredList: [Idea] {
+		guard !searchText.isEmpty else { return list }
+		return list.filter { $0.idea.localizedCaseInsensitiveContains(searchText) }
+	}
+	
+	var body: some View {
+		VStack {
+			List(filteredList) { idea in
+				Text(idea.idea)
+			}
+			.searchable(text: $searchText, placement: .navigationBarDrawer)
+			.sheet(isPresented: $showingSheet, content: ExampleSheet.init)
+			
+			Spacer()
+			
+			Button {
+				showingSheet = true
+			} label: {
+				Text("Propose your own Idea")
+			}
+			
 		}
-		.searchable(text: $searchText, placement: .navigationBarDrawer)
-    }
+	}
+}
+
+struct ExampleSheet: View {
+	@Environment(\.dismiss) var dismiss
+	@State var title: String = ""
+	@State var description: String = ""
+	
+	var body: some View {
+		NavigationStack {
+			VStack {
+				Form {
+					Section(header: Text("Title")) {
+						TextField("Your idea in a few words", text: $title)
+					}
+					
+					Section(header: Text("Description"), footer: Text("0/255")) {
+						TextField("Describe your idea …", text: $description, axis: .vertical)
+										.lineLimit(2...)
+//						TextEditor(text: $description)
+					}
+				}
+				
+			}
+			.navigationTitle("Your Idea")
+			.toolbar(content: {
+				ToolbarItem(placement: .topBarLeading) {
+					Button {
+						dismiss()
+					} label: {
+						Text("Cancel")
+					}
+				}
+				ToolbarItem(placement: .topBarTrailing) {
+					Button {
+						dismiss()
+					} label: {
+						Text("Send")
+					}
+				}
+			})
+		}
+	}
 }
 
 #Preview {
